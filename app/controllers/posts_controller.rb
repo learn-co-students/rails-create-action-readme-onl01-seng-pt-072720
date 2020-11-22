88b4b14d2d39ad3be13154152bc274db1b89ae22
fentyhall <fentymhall@gmail.com>
2020-11-22 11:07:59 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,12 @@ class PostsController < ApplicationController
   end
 
   # add create method here
+  # redirect to the new resource's show page
+  def create 
+    @post = Post.new 
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end 
 end
